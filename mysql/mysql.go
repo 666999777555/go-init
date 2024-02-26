8dbcee058a903b7214c6c40d5d0a9283fedeeaad
“仲鹏宇” <13086242+zhong-pengyuzpy@user.noreply.gitee.com>
2024-02-26 18:51:58 +0800

    qnmd

---
+++ mysql/mysql.go
@@ -0,0 +1,16 @@
+package mysql
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func InitMysql() error {
+	var err error
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	return err
+}
