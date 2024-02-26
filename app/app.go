e09eedbf25b4badad863e23712f26f36dc9f4f15
“仲鹏宇” <13086242+zhong-pengyuzpy@user.noreply.gitee.com>
2024-02-26 19:04:59 +0800

    第一次

--- app/app.go
+++ app/app.go
@@ -1,1 +1,14 @@
 package app
+
+import "config/mysql"
+
+func Init(apps ...string) error {
+	var err error
+	for _, val := range apps {
+		switch val {
+		case "mysql":
+			err = mysql.InitMysql()
+		}
+	}
+	return err
+}
