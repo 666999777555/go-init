6255d4ac7697f68d3874d26dbbdf1530f17df9b0
“仲鹏宇” <13086242+zhong-pengyuzpy@user.noreply.gitee.com>
2024-02-26 19:50:31 +0800

    无mod

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -5,12 +5,12 @@ import (
 	"gorm.io/gorm"
 )
 
-var Db *gorm.DB
+var DB *gorm.DB
 
 func InitMysql() error {
 	var err error
 	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "root:root@tcp(127.0.0.1:3306)/2108a?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	return err
 }
