c1d4c219857005ad79e72104afce88e47a58bb44
“仲鹏宇” <13086242+zhong-pengyuzpy@user.noreply.gitee.com>
2024-03-01 16:21:41 +0800

    nacos

--- grpc/main.go
+++ grpc/main.go
@@ -2,25 +2,47 @@ package register
 
 import (
 	"fmt"
+	"github.com/666999777555/go-init/config"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"gopkg.in/yaml.v3"
 	"log"
 	"net"
 )
 
-func Register(port int, res func(s *grpc.Server)) error {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+type Config struct {
+	App struct {
+		Ip   string `yaml:"ip"`
+		Port string `yaml:"port"`
+	} `yaml:"app"`
+}
+
+func getConfig(serviceName string) (*Config, error) {
+	ConfigInfo, err := config.GetConfig("DEFAULT_GROUP", serviceName)
 	if err != nil {
-		log.Fatalf("failed to listen:%v", err)
+		return nil, err
+	}
+	cnf := new(Config)
+	err = yaml.Unmarshal([]byte(ConfigInfo), cnf)
+	if err != nil {
+		return nil, err
+	}
+	return nil, err
+}
+
+func Register(serviceName string, res func(s *grpc.Server)) error {
+	cof, err := getConfig(serviceName)
+	if err != nil {
 		return err
 	}
-	s := grpc.NewServer()
-	//反射接口支持查询
-	reflection.Register(s)
-	res(s)
-	log.Printf("sever listening at %v", err)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to listen:%v\",err")
+	lis, err := net.Listen("tcp", fmt.Sprintf("%V:%v", cof.App.Ip, cof.App.Port))
+	if err != nil {
+		log.Fatalf(err.Error())
+		return err
+	}
+	g := grpc.NewServer()
+	log.Printf("sever listening at %v", lis.Addr())
+	if err := g.Serve(lis); err != nil {
+		log.Fatalf(err.Error())
 		return err
 	}
 	return err
